83749026022c0723e08a8c5051952c53d5ce5335
Sebastian Lalek <chemiczq@gmail.com>
2023-11-05 15:47:05 +0100

    add ImageGalleryItem component

---
+++ src/components/imagegalerryitem/imagegalleryitem.js
@@ -0,0 +1,11 @@
+function ImageGalleryItem({ image }) {
+  return (
+    <li className="ImageGalleryItem">
+      <img
+        className="ImageGalleryItem-image"
+        src={image.webformatURL}
+        alt={image.tags}
+      />
+    </li>
+  );
+}
